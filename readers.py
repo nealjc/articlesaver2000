896fdac16fdc17e3ee64dc8b241c2549aa4c5f60
Neal <ncharbonneau@gmail.com>
2012-07-18 20:50:03 -0400

    initial commit

---
+++ readers.py
@@ -0,0 +1,114 @@
+"""
+Module to send urls to Pocket and Instapapper.
+"""
+
+import json
+import requests
+import time
+import logging
+
+_POCKET_KEY="dhuddrmvT6624y80g0Ab02eS2bg4I8fI"
+#1 request every rate seconds
+_INSTAPAPPER_RATE = 0.5
+
+BAD_CREDENTIALS = 0
+REQ_ERROR = 1
+SERVICE_DOWN = 2
+SUCCESS = 3
+
+module_logger = logging.getLogger('readers')
+
+def send_to_pocket(urls, name, pw):
+    """Sends a list of urls to Pocket
+    with the given name and password.
+
+    urls - a list of tuples [(url, title), ...]
+    name - string, username
+    pw - string, username
+
+    Returns status code to indicate success/failure
+    """    
+    new_urls = {}
+    for idx, url in enumerate(urls):
+        if not url[0].startswith("http://"):
+            formatted_url = "http://" + url[0]
+        else:
+            formatted_url = url[0]
+        new_urls["{0}".format(idx)] = {"url":formatted_url,
+                                       "title":url[1]}
+    params = {}
+    params['username'] = name
+    params['password'] = pw
+    params['apikey'] = _POCKET_KEY
+    params['new'] = json.dumps(new_urls)
+    debug_params = params.copy()
+    del debug_params['password']
+    module_logger.info("Sending request to Pocket {0}".format(debug_params))
+    resp = requests.post("https://readitlaterlist.com/v2/send",
+                     data=params)
+    err = resp.headers.get('X-Error', None)
+    if resp.status_code == 200:
+        module_logger.info("Request successful")
+        return SUCCESS
+    elif resp.status_code == 400:
+        module_logger.error("Invalid request: {0}".format(err))
+        return REQ_ERROR
+    elif resp.status_code == 401:
+        module_logger.error("Invalid username")
+        return BAD_CREDENTIALS
+    elif resp.status_code == 403:
+        module_logger.error("Rate limit exceeded")
+        return REQ_ERROR
+    elif resp.status_code == 503:
+        module_logger.error("Pocket server is temp. unavailable")
+        return SERVICE_DOWN
+    else:
+        module_logger.error("Unable to contact server")
+        return SERVICE_DOWN
+
+def send_to_instapapper(urls, name, pw):
+    """Sends a list of urls to instapapper
+    with the given name and password.
+    The function pauses between each call, so the runtime
+    is proportional to the number of urls.
+
+    urls - a list of tuples [(url, title), ...]
+    name - string, username
+    pw - string, username
+
+    Returns a two tuple (status, [failed urls])
+    """
+    params = {}
+    params['username'] = name
+    params['password' ] = pw
+    while urls:
+        params['url'] = urls[0][0]
+        params['title'] = urls[0][1]
+        debug_params = params.copy()
+        del debug_params['password']
+        module_logger.info("Sending request to instapapper {0}".format(
+            debug_params))
+        resp = requests.post("https://www.instapaper.com/api/add", data=params)
+        if resp.status_code == 201:
+            module_logger.info("Successful")
+        elif resp.status_code == 400:
+            module_logger.error("Invalid request")
+            return (REQ_ERROR, urls)
+        elif resp.status_code == 403:
+            module_logger.error("Bad credentials")
+            return (BAD_CREDENTIALS, urls)
+        elif resp.status_code == 500:
+            module_logger.error("Temporarily unavailable")
+            return (SERVICE_DOWN, urls)
+        else:
+            #failed to contact server at all
+            module_logger.error("Unable to contact server {0}".format(
+                resp.status_code))
+            return (SERVICE_DOWN, urls)
+        urls.pop(0)
+        time.sleep(_INSTAPAPPER_RATE)
+
+    return (SUCCESS, [])
+
+if __name__ == '__main__':
+    logging.basicConfig("%(asctime)-15s %(message)s")
